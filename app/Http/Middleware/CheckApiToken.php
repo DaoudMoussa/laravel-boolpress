4d951adeb9fc9bed33559ccb7ca7b00509b39c77
Daoud <99m.daoud@gmail.com>
2021-02-03 21:00:23 +0100

    aggiunto middleware alla api

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\User;
 
 class CheckApiToken
 {
@@ -15,6 +16,24 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
+
+        $authToken = $request->header('authorization');
+        if (!$authToken) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Non è presente un api token'
+            ]);
+        }
+
+        $apiToken = substr($authToken, 7);
+        $user = User::where('api_token', $apiToken)->first();
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'error' => "L'api token è errata"
+            ]);
+        }
+
         return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', 'Api\HomeController@index');
+Route::post('/posts', 'Api\HomeController@index')->middleware('apiToken');
